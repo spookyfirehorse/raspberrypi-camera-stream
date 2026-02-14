cb54037bc521d371ac72f6c58678987851596883
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-02-14 16:48:30 +0100

    Update home-stream-rpicam-lucy-realtime-final.sh

--- home-stream-rpicam-lucy-realtime-final.sh
+++ home-stream-rpicam-lucy-realtime-final.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-stdbuf -oL -eL chrt -f 90 taskset -c 0,1 rpicam-vid --flush --low-latency --verbose 0 \
+
+PULSE_LATENCY_MSEC=5 stdbuf -o0 -e0  chrt -f 90 taskset -c 0,1  rpicam-vid --flush   -b 1500000    --denoise cdn_off   --codec libav --libav-format mpegts \
+--profile=high  --hdr=off --level 4.1 --framerate 25  --width 1280 --height 720   --av-sync=0 \
+--autofocus-mode manual --autofocus-range normal --autofocus-window  0.25,0.25,0.5,0.5 \
+--audio-codec libopus --audio-samplerate 48000 --shutter 20000 --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx708.json  \
+--audio-channels 2 --libav-audio 1 --audio-source pulse  --awb indoor -t 0 --intra 25 \
+--inline  -n  -o  - | chrt -f 90 taskset -c 3  ffmpeg   -loglevel warning  -hide_banner -fflags nobuffer+genpts+flush_packets \
+-hwaccel drm -hwaccel_output_format drm_prime -f mpegts  -i -  -metadata title='lucy' -c copy -copyts \
+-fps_mode passthrough   -flags low_delay -avioflags direct -map 0:0 -map 0:1 -muxdelay 0  -f rtsp -buffer_size 4k \
+-rtsp_flags filter_src -tcp_nodelay 1  -rtsp_transport tcp -pkt_size 1316
+
+
+
+stdbuf -o0 -e0 chrt -f 90 taskset -c 0,1 rpicam-vid --flush --low-latency --verbose 0 \
 --denoise cdn_off -t 0 --width 1280 --height 720 --framerate 25 \
 --autofocus-mode manual --autofocus-range normal \
 --autofocus-window 0.25,0.25,0.5,0.5 \
@@ -18,7 +31,7 @@ PULSE_LATENCY_MSEC=10 chrt -f 90 taskset -c 3 ffmpeg -y -loglevel warning -hwacc
 rtsp://"user:pwd"@"localhost:8557"/mystream > /dev/null 2>&1
 
 
-stdbuf -oL -eL chrt -f 90 taskset -c 0,1  rpicam-vid  --flush  -b 1500000    --denoise cdn_off   --codec libav --libav-format mpegts \
+stdbuf -o0 -e0 chrt -f 90 taskset -c 0,1  rpicam-vid  --flush  -b 1500000    --denoise cdn_off   --codec libav --libav-format mpegts \
 --profile=high  --hdr=off --level 4.1 --framerate 25  --width 1280 --height 720   --av-sync=0 \
 --autofocus-mode manual --autofocus-range normal --autofocus-window  0.25,0.25,0.5,0.5 --audio-codec libopus \
 --audio-channels 2 --libav-audio 1 --audio-source pulse  --awb indoor -t 0 --intra 25 \
@@ -32,7 +45,7 @@ stdbuf -oL -eL chrt -f 90 taskset -c 0,1  rpicam-vid  --flush  -b 1500000    --d
 # -sws_flags fast_bilinear
 
 ```bash
-stdbuf -oL -eL chrt -f 90 taskset -c 0,1  rpicam-vid --denoise cdn_off -t 0 --width 1280 --height 720 --framerate 25 \
+stdbuf -o0 -e0 chrt -f 90 taskset -c 0,1  rpicam-vid --denoise cdn_off -t 0 --width 1280 --height 720 --framerate 25 \
 --autofocus-mode manual --autofocus-range normal --autofocus-window 0.25,0.25,0.5,0.5 \
 --libav-video-codec h264_v4l2m2m --libav-format h264 --codec libav --inline \
 --awb indoor --profile high --level 4.0 --intra 25 -b 1500000 -n -o - | \
