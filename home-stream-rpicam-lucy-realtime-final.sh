b91995994a0e2d6a4b9de4b892b6189fdb5638c4
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-02-14 16:50:36 +0100

    Update home-stream-rpicam-lucy-realtime-final.sh

--- home-stream-rpicam-lucy-realtime-final.sh
+++ home-stream-rpicam-lucy-realtime-final.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# realtime kernel realtime audio config
+
+
 PULSE_LATENCY_MSEC=5 stdbuf -o0 -e0  chrt -f 90 taskset -c 0,1  rpicam-vid --flush   -b 1500000    --denoise cdn_off   --codec libav --libav-format mpegts \
 --profile=high  --hdr=off --level 4.1 --framerate 25  --width 1280 --height 720   --av-sync=0 \
 --autofocus-mode manual --autofocus-range normal --autofocus-window  0.25,0.25,0.5,0.5 \
@@ -10,7 +13,19 @@ PULSE_LATENCY_MSEC=5 stdbuf -o0 -e0  chrt -f 90 taskset -c 0,1  rpicam-vid --flu
 -fps_mode passthrough   -flags low_delay -avioflags direct -map 0:0 -map 0:1 -muxdelay 0  -f rtsp -buffer_size 4k \
 -rtsp_flags filter_src -tcp_nodelay 1  -rtsp_transport tcp -pkt_size 1316
 
+ # normal also very quick
 
+PULSE_LATENCY_MSEC=15 stdbuf -o0 -e0  nice -n -11  taskset -c 0,1  rpicam-vid --flush   -b 1500000    --denoise cdn_off   --codec libav --libav-format mpegts \
+--profile=high  --hdr=off --level 4.1 --framerate 25  --width 1280 --height 720   --av-sync=0 \
+--autofocus-mode manual --autofocus-range normal --autofocus-window  0.25,0.25,0.5,0.5 \
+--audio-codec libopus --audio-samplerate 48000 --shutter 20000 --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx708.json  \
+--audio-channels 2 --libav-audio 1 --audio-source pulse  --awb indoor -t 0 --intra 25 \
+--inline  -n  -o  - | nice -n -11 taskset -c 3  ffmpeg   -loglevel warning  -hide_banner -fflags nobuffer+genpts+flush_packets \
+-hwaccel drm -hwaccel_output_format drm_prime -f mpegts  -i -  -metadata title='devil' -c copy -copyts \
+-fps_mode passthrough   -flags low_delay -avioflags direct -map 0:0 -map 0:1 -muxdelay 0  -f rtsp -buffer_size 4k \
+-rtsp_flags filter_src -tcp_nodelay 1  -rtsp_transport tcp -pkt_size 1316
+
+# realtime
 
 stdbuf -o0 -e0 chrt -f 90 taskset -c 0,1 rpicam-vid --flush --low-latency --verbose 0 \
 --denoise cdn_off -t 0 --width 1280 --height 720 --framerate 25 \
@@ -30,6 +45,7 @@ PULSE_LATENCY_MSEC=10 chrt -f 90 taskset -c 3 ffmpeg -y -loglevel warning -hwacc
 -f rtsp -rtsp_transport tcp -tcp_nodelay 1 -rtsp_flags filter_src -muxdelay 0 -flags +low_delay -avioflags direct -pkt_size 1316 \
 rtsp://"user:pwd"@"localhost:8557"/mystream > /dev/null 2>&1
 
+# normal quick
 
 stdbuf -o0 -e0 chrt -f 90 taskset -c 0,1  rpicam-vid  --flush  -b 1500000    --denoise cdn_off   --codec libav --libav-format mpegts \
 --profile=high  --hdr=off --level 4.1 --framerate 25  --width 1280 --height 720   --av-sync=0 \
